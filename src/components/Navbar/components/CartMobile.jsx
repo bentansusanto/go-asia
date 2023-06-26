2a8f9306c4f6bfefc89d65e016b0edd6570ec77e
bentansusanto <tansusanto194@gmail.com>
2023-06-27 01:08:20 +0700

    update page

--- src/components/Navbar/components/CartMobile.jsx
+++ src/components/Navbar/components/CartMobile.jsx
@@ -4,7 +4,7 @@
 const CartMobile = ({openCart}) => {
   return (
     <div className={`${openCart ? "bg-[#0d0428] top-20 py-5 px-3" : "hidden"}  w-screen h-auto fixed transition-all ease-linear duration-500`}>
-        <p>Cart not found</p>
+        <p>Cart not found !</p>
     </div>
   )
 }
