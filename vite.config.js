e8bcd6b5f32d468549a64437eebd7c0142b9e6f8
bentansusanto <tansusanto194@gmail.com>
2023-07-08 12:25:34 +0700

    solving 404

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
-    proxy : {
-      '/[^.]+' : {
-        target : '/',
-        rewrite : (path) => path === '/' ? '/' : '/'
-      }
-    }
-  }
-})
+  routes: [
+    {
+      src: '/[^.]+',
+      dest: '/',
+      status: 200,
+    },
+  ],
+});
